17fd21c98cf96032fa1eb3f28d1db9e1008bc179
Marc Munene <marcmunene104@gmail.com>
2025-02-14 12:37:37 +0300

    routes

---
+++ Server/src/routes/usersRoute.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getUsers } from "../controllers/users.js";
+
+const usersRouter = Router();
+
+usersRouter.route("/users").get(getUsers);
+
+export { usersRouter };
--- Server/src/server.js
+++ Server/src/server.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { deleteBlogs, editBlogs, getBlogs,  } from "./controllers/blogs.js";
+import { deleteBlogs, editBlogs, getBlogs } from "./controllers/blogs.js";
 import { blogRouter } from "./routes/blogRoute.js";
 import { getHome } from "./controllers/Home.js";
+import { usersRouter } from "./routes/usersRoute.js";
 
 const app = express();
 
@@ -9,7 +10,8 @@ const PORT = process.env.PORT;
 
 app.get("/", getHome);
 
-app.use("/api", blogRouter)
+
+app.use("/api", blogRouter , usersRouter);
 
 /*
 app.get("/blogs", getBlogs);
