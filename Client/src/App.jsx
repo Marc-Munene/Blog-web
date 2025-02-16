7215ed589da51cf5a7324a54ff79e57a4eeaf075
Marc Munene <marcmunene104@gmail.com>
2025-02-16 12:08:08 +0300

    Client

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,1 +1,11 @@
- 
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-9xl text-black">My name is munene</h1>
+    </div>
+  );
+};
+
+export { App };
